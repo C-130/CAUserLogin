e5d4bc03808a38c885c6425900cd9fa361fd903e
Mingxiao Wei <mingxiaowei@mingxiaos-air.home>
2024-10-19 20:23:31 -0400

    Completed Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set the current user as the user with username name.
+     * @param name the username to look up
+     */
+    void setCurrentUser(String name);
 }
